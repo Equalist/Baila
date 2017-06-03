e82d5bd9c085aae9c9ada7d38ff5887dc1096cec
The Equalist Amon <Amon@execs.com>
2017-06-03 17:14:26 -0400

    Update sql.php

--- includes/sql.php
+++ includes/sql.php
@@ -4,10 +4,11 @@ function db_query($sql)
     $args = func_get_args(); 
     $dbc = $GLOBALS['db_conn']; 
     $stmt = $dbc->prepare($sql); 
-    for ($i = 1; $i < func_num_args(); $i++) 
+    for ($i = 1; $i < func_num_args(); $i++) {
         $stmt->bindParam(":$i", $args[$i]); 
+    }
     try { $stmt->execute(); } 
-    catch(PDOException $e) { echo "<div id=\"error\">Database error:".$e->getMessage()."</div>"; } 
+    catch(PDOException $e) { echo "<div id='error'>Database Error:{$e->getMessage()}</div>"; } 
     return $stmt; 
 }
 
